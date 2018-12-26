bc3239639b758586fb850a330248837ff10ae4f4
Wenhao Ni <niwenhao@gmail.com>
2018-12-26 09:32:28 +0900

    u

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import URLDecoder from './com/url-decoder'
 import JSONFormater from './com/json-formater'
+
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+const Home = () => <div></div>
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <URLDecoder/>
-        <JSONFormater/>
-      </div>
+        <Router>
+          <div style={{
+            width: "950px",
+            margin: "auto"
+          }}>
+            <div style={{
+              fontSize: 30,
+              width: "200px",
+              height: "80px",
+              margin: "auto",
+            }}>My Toolbox</div>
+            <nav style={{
+              float: "left",
+              width: "180px",
+              textAlign: "left"
+            }}>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/url-decode">Decode a URL</Link></li>
+                <li><Link to="/json-format">Format a Json</Link></li>
+              </ul>
+            </nav>
+            <div style={{
+              float: "right",
+              width: "700px"
+            }}>
+              <Route path='/' component={Home} />
+              <Route path='/url-decode' component={URLDecoder} />
+              <Route path='/json-format' component={JSONFormater} />
+            </div>
+          </div>
+
+        </Router>
     );
   }
 }
